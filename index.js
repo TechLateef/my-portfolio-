35cfe6b18ce075509ac9da0f7a1f9e157b824a95
Your Name <you@example.com>
2022-08-29 05:48:45 -0700

    Still on proccess

---
+++ index.js
@@ -0,0 +1,12 @@
+// $('project-btn').on('click',function(){
+//     $('p-section').slideup().slidedown().animate({opacity:0.5})
+// })
+
+// window.addEventListener('scroll',()=> {
+//   $('.about').slideUp().slideDown(h )
+// })
+
+
+// function myfunction(){
+//   $('.blue').slideUp().slideDown();
+// };
