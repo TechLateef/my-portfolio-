05c064ada9563ae47d2451a840cb5c4660ed32e8
Your Name <you@example.com>
2022-08-29 07:33:58 -0700

    still on Process

--- index.js
+++ index.js
@@ -10,3 +10,20 @@
 // function myfunction(){
 //   $('.blue').slideUp().slideDown();
 // };
+
+window.addEventListener('scroll', reveal);
+function reveal(){
+var reveals = $('.reveal');
+for(i = 0; i < reveals.length; i ++){
+    var windowHeight = window.innerHeight;
+    var revealtop = reveals[i].getBoundingClientRect().top;
+    var revealpoint = 150;
+
+    if(revealtop < windowHeight - revealpoint){
+        reveals[i].classList.add('active')
+    }else{
+        reveals[i].classList.remove('active')
+    }
+}
+
+}
